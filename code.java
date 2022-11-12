12368cc0740e3f7b9a63c1c693f2d73411c8365b
pawannnagar <pawannagar08011998@gmail.com>
2022-11-12 12:35:41 +0530

    my java code

---
+++ code.java
@@ -0,0 +1,22 @@
+class code
+{
+public static void main(String[] args)
+{
+int i=5;
+int j=5;
+int k=5;
+
+for(i=1;i<=5;i++)
+{
+
+for(j=4;j>=i;j--)
+{ 
+System.out.print(k);
+}
+k--;
+System.out.println("A");
+System.out.print("_");
+}
+
+}
+}
